59818a73aa7ea47dc7c4c862fa42e0d621f85c21
Karoon Sillapapan <karoon@ananda.co.th>
2021-05-11 11:30:26 +0700

    demo func

--- go-basic/main.go
+++ go-basic/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"updev/gobasic/functions"
 	"updev/gobasic/variable"
 )
 
+// main func is entry function of go app
 func main() {
 	fmt.Println("main action")
+
+	fmt.Println("[---- Variable Demo ----]")
 	variable.Demo()
+
+	demoFunc()
+
+}
+
+func demoFunc() {
+	fmt.Println("[---- Function Demo ----]")
+	functions.RunDemoFunction()
+	functions.PublicFunc()
 
 }
