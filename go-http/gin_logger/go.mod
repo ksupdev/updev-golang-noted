f1b4252fc53bb7e69c5e2e56abe70a46a340ddf8
Karoon Sillapapan <karoon@ananda.co.th>
2021-05-17 14:19:44 +0700

    basic logs

--- go-http/gin_logger/go.mod
+++ go-http/gin_logger/go.mod
@@ -1,3 +1,5 @@
 module updev/gin-logger
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.1 // indirect
--- go-http/gin_logger/index.go
+++ go-http/gin_logger/index.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default()
+
+	runningDir, _ := os.Getwd()
+
+	errlogfile, _ := os.OpenFile(fmt.Sprintf("%s/logs/gin_error.log", runningDir), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	accesslogfile, _ := os.OpenFile(fmt.Sprintf("%s/logs/gin_access.log", runningDir), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+
+	gin.DefaultErrorWriter = errlogfile
+	gin.DefaultWriter = accesslogfile
+
+	r.Use(gin.Logger())
+
+	r.GET("/", func(c *gin.Context) {
+		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte("root"))
+	})
+
+	r.GET("/profile", func(c *gin.Context) {
+		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte("profile"))
+	})
+
+	r.Run(":85")
 
 }
