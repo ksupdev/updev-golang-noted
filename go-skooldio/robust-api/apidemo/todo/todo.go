8124aba64320c04246e5561f503ad5d1e79f00a0
karoon sillapapan <karoons.updev@gmail.com>
2022-08-01 16:56:38 +0700

    init todo package

---
+++ go-skooldio/robust-api/apidemo/todo/todo.go
@@ -0,0 +1,32 @@
+package todo
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Todo struct {
+	Title string `json:"text"`
+}
+
+func (Todo) Tablename() string {
+	return "todos "
+}
+
+type TodoHandler struct {
+}
+
+func NewTodoHandler() *TodoHandler {
+	return &TodoHandler{}
+}
+
+func (t *TodoHandler) NewTask(c *gin.Context) {
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+}
